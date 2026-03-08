d6932aa6fbc9dbe98d9fe115cd58dc580fd7a9ce
Koyeb App <koyebapp@gmail.com>
2026-03-08 20:23:43 +0500

    Add support for SQLite in database availability check

--- lib/index.ts
+++ lib/index.ts
@@ -10,7 +10,8 @@ import store from './lightweight_store.js';
 const MONGO_URL = process.env.MONGO_URL;
 const POSTGRES_URL = process.env.POSTGRES_URL;
 const MYSQL_URL = process.env.MYSQL_URL;
-const HAS_DB = !!(MONGO_URL || POSTGRES_URL || MYSQL_URL);
+const SQLITE_URL = process.env.DB_URL;
+const HAS_DB = !!(MONGO_URL || POSTGRES_URL || MYSQL_URL || SQLITE_URL);
 
 const dataPath = dataFile('userGroupData.json');
 
