db6cf22df73913a313224976d69134a8d9a63363
macksyn <155353088+macksyn@users.noreply.github.com>
2026-07-13 01:54:02 +0100

    Changed the Mega caption to Groq

--- plugins/video.ts
+++ plugins/video.ts
@@ -89,7 +89,7 @@ export default {
                 video: { url: videoData.downloadUrl },
                 mimetype: 'video/mp4',
                 fileName: `${videoData.title || videoTitle || 'video'}.mp4`,
-                caption: `🎬 *${videoData.title || videoTitle || 'Video'}*\n\n> *_Downloaded by MEGA-MD_*`
+                caption: `🎬 *${videoData.title || videoTitle || 'Video'}*\n\n> _Groq™_*`
             }, { quoted: message });
 
         } catch (err: any) {
